e6f1fc8f9596b2eb073ca41aa0b629d257dee347
Brian Dorton <dorton@gmail.com>
2017-09-13 17:51:35 -0500

    add test coverage

--- test/models/finder_test.rb
+++ test/models/finder_test.rb
@@ -9,7 +9,7 @@ end
 class FinderTest < ActiveSupport::TestCase
 
   test "imports the right amount of data" do
-    VCR.use_cassette("results") do
+    VCR.use_cassette("results", :record => :new_episodes) do
       results = Finder.new.search("onion")
       assert_equal 10, results.count
     end
@@ -17,8 +17,22 @@ class FinderTest < ActiveSupport::TestCase
 
 
   test "finder with blank search" do
-    VCR.use_cassette("results") do
+    VCR.use_cassette("results", :record => :new_episodes) do
       results = Finder.new.search("")
+      assert_equal 0, results.count
+    end
+  end
+
+  test "finder with comma in search search" do
+    VCR.use_cassette("results", :record => :new_episodes) do
+      results = Finder.new.search("onion, potato")
+      assert_equal 10, results.count
+    end
+  end
+
+  test "known queries that throw an error" do
+    VCR.use_cassette("results", :record => :new_episodes) do
+      results = Finder.new.search("eggs bacon")
       assert_equal 0, results.count
     end
   end
